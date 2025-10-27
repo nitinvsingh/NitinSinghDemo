5e3ee057e17c029eef8662b103130a8854cd23b9
Nitin Kumar Singh <nitinvsingh@gmail.com>
2025-10-28 00:32:21 +0530

    Add mock dataset for holdings and added a Holdings model

---
+++ NitinSinghDemo/ListHoldings/Holding.swift
@@ -0,0 +1,16 @@
+//
+//  Holding.Swift
+//  NitinSinghDemo
+//
+//  Created by Nitin Kumar Singh on 27/10/25.
+//
+
+import Foundation
+
+struct Holding {
+    let symbol: String
+    let quantity: Int
+    let lastTradedPrice: Double
+    let averagePrice: Double
+    let closingPrice: Double
+}
