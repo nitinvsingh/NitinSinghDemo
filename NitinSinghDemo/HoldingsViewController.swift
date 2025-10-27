3b885678630f77f38163d0b541b4af631d07ec82
Nitin Kumar Singh <nitinvsingh@gmail.com>
2025-10-27 18:09:17 +0530

    Setting up the app - Deleted the main.storyboard and removed all its references. This includes from the project config, scene configuration in info.plist, etc. - Configured SceneDelegate to present a window and the root view controller. - Removed code not required for now from AppDelegate and SceneDelegate files. - Added accent color asset, app icon. - Configured the LaunchScreen storyboard.

---
+++ NitinSinghDemo/HoldingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HoldingsViewController.swift
+//  NitinSinghDemo
+//
+//  Created by Nitin Kumar Singh on 27/10/25.
+//
+
+import UIKit
+
+class HoldingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Portfolio"
+        view.backgroundColor = .systemBackground
+    }
+}
